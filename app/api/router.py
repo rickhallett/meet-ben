25742f3605471c01f2c29f4d539e5e3ea95955bf
Rick Hallett <rickhallett@icloud.com>
2025-01-18 15:30:41 +0000

    fix: fix router import api

--- app/api/router.py
+++ app/api/router.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter
 
-from api import endpoint
 from api.endpoint import router as endpoint_router
 
 """
@@ -10,4 +9,6 @@ This module sets up the API router and includes all defined endpoints.
 It uses FastAPI's APIRouter to group related endpoints and provide a prefix.
 """
 
-endpoint_router.include_router(endpoint.router, prefix="/api/ben", tags=["events"])
+router = APIRouter()
+
+router.include_router(endpoint_router, prefix="/api/ben")
