a174c4898446690b4f3d8db7ecc8e647a920685c
Rick Hallett <rickhallett@icloud.com>
2025-01-29 18:44:34 +0000

    fix: greet user logic

--- app/api/dependencies.py
+++ app/api/dependencies.py
@@ -1,5 +1,6 @@
 import logging
 from typing import Generator
+from contextlib import contextmanager
 
 from database.session import SessionLocal
 from sqlalchemy.orm import Session
@@ -13,6 +14,26 @@ def db_session() -> Generator:
     """
     session: Session = SessionLocal()
     try:
+        yield session
+        session.commit()
+    except Exception as ex:
+        session.rollback()
+        logging.error(ex)
+        raise ex
+    finally:
+        session.close()
+
+
+@contextmanager
+def db_session_ctx():
+    """Database Session Dependency.
+
+    This function provides a database session for each request.
+    It ensures that the session is committed after successful operations.
+    """
+    session: Session = SessionLocal()
+    try:
+        session.expire_on_commit = False  # TODO: check this is safe
         yield session
         session.commit()
     except Exception as ex:
